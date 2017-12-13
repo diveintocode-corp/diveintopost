6bd7f7582c3af3c7525f51f2e8b721f38f079016
Yoshihiro Kimura <kimura@diveintocode.jp>
2017-12-13 16:05:14 +0900

    Fix configure

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,25 @@ Bundler.require(*Rails.groups)
 
 module Keijiban
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
+    config.paths.add 'lib', eager_load: true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: true,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.assets     false
+      g.helper     false
+    end
   end
 end
