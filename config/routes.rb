a47a55bd82f402dc85de03e5186d7d44acef492c
Yoshihiro Kimura <kimura@diveintocode.jp>
2017-12-15 18:56:54 +0900

    ログイン周りの画面調整等

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'users#dashboard'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
 
   resources :users do
     get :dashboard, on: :collection
