5d3bc9aa71232bc35e461b61a5903cb593dfc1c0
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-06-22 13:53:22 +0900

    Rspecの初期設定完了？？

--- spec/controllers/statics_controller_spec.rb
+++ spec/controllers/statics_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StaticsController, type: :controller do
   describe 'GET #top' do
+    login_user
     it 'returns http success' do
       get :top
       expect(response).to have_http_status(:success)
