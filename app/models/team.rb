c5153b6e8f15e747f46a9be10d2848f21fca1f47
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-06-26 06:17:47 +0900

    RubocopのInverseOfとHasManyOrHasOneDependentをfalseに設定

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  belongs_to :owner, class_name: 'User', foreign_key: :owner_id, inverse_of: :assign
+  belongs_to :owner, class_name: 'User', foreign_key: :owner_id
 
   has_many :assigns, dependent: :destroy
   has_many :members, through: :assigns, source: :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :teams, foreign_key: :owner_id, dependent: :destroy, inverse_of: :assign
+  has_many :teams, foreign_key: :owner_id
   has_many :assigns, dependent: :destroy
   has_many :teams, through: :assigns
   has_many :articles, dependent: :destroy
