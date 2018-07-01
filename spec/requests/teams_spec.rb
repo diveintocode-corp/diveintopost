54225b3cb1069bf3f6e10e474c7863e94e0fc248
kazoo <kazuki_saito@diveintocode.jp>
2018-07-01 18:55:12 +0900

    specs modified for rspec

--- spec/requests/teams_spec.rb
+++ spec/requests/teams_spec.rb
@@ -1,12 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe 'Teams', type: :request do
-  describe 'GET /teams' do
-    it 'works! (now write some real specs)' do
-      user = FactoryBot.create(:user)
-      sign_in user
-      get teams_path
-      expect(response).to have_http_status(200)
-    end
-  end
 end
