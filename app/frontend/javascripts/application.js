944686c5e8c5d6dc70894b4dff96107b785d3645
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-07-06 17:19:21 +0900

    font-awesomeを動くようにした

--- app/frontend/javascripts/application.js
+++ app/frontend/javascripts/application.js
@@ -2,6 +2,6 @@ import 'stylesheets/application';
 import Rails from 'rails-ujs';
 Rails.start();
 
-import 'jquery/dist/jquery.min.js'
+import jQuery from 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'admin-lte/dist/js/adminlte.min.js'
