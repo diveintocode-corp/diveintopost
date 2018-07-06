1607a15c738fcdebe181e8ccf07e0e02388bcf1b
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-07-06 17:43:27 +0900

    読み込むadminLTEのcssファイル名を修正

--- app/frontend/javascripts/application.js
+++ app/frontend/javascripts/application.js
@@ -1,7 +1,5 @@
 import 'stylesheets/application';
-import Rails from 'rails-ujs';
-Rails.start();
-
-import jQuery from 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'admin-lte/dist/js/adminlte.min.js'
+import Rails from 'rails-ujs';
+Rails.start();
