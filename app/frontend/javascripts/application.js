b52c6249c16fb7c741a3b498e44c2f3f3e886d2f
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-07-06 12:03:45 +0900

    AdminLTEの大枠導入

--- app/frontend/javascripts/application.js
+++ app/frontend/javascripts/application.js
@@ -1,3 +1,7 @@
 import 'stylesheets/application';
 import Rails from 'rails-ujs';
 Rails.start();
+
+import 'jquery/dist/jquery.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'admin-lte/dist/js/adminlte.min.js'
