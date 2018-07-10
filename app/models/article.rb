9b61773fd28cc9039e686dc2fa818ea5d8ea037b
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-07-10 14:56:23 +0900

    article のバリデーション実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 1 }, length: { maximum: 100 }
+  validates :content, presence: true, length: {minimum: 1 }, length: { maximum: 1000 }
+
   belongs_to :user
   belongs_to :team
   belongs_to :agenda
