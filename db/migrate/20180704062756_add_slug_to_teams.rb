257c4aecbaef21d297ad65773234c4759fbca274
kazoo <kazuki_saito@diveintocode.jp>
2018-07-12 20:18:24 +0900

    refactor: removed migration file which is unnecessary.

--- db/migrate/20180704062756_add_slug_to_teams.rb
+++
@@ -1,6 +0,0 @@
-class AddSlugToTeams < ActiveRecord::Migration[5.2]
-  def change
-    add_column :teams, :slug, :string, null: false
-    add_index :teams, :slug, unique: true
-  end
-end
--- db/migrate/20180704105239_remove_column_from_teams.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromTeams < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :teams, :slug, :string, null: false
-  end
-end
