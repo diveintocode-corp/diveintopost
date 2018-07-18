00598125bcd7cd8ab42d76f08a1ca382dba564f0
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-07-18 11:22:33 +0900

    サイドバーにagendaとarticleが表示されるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_team, if: :user_signed_in?
+
+  private
+
+  # FIXME: 暫定的にteamに値を入れる処理をかませる
+  def set_team
+    @team = current_user.teams.first
+  end
 end
