b7f985ba9033ba15decff89ed31003b9c00072b1
Yoshihiro Kimura <kimura@diveintocode.jp>
2018-07-18 15:16:09 +0900

    サイドバーに使うteamの変数に別名をつけてバッティングしないようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :set_team, if: :user_signed_in?
+  before_action :set_working_team, if: :user_signed_in?
 
   private
 
   # FIXME: 暫定的にteamに値を入れる処理をかませる
-  def set_team
-    @team = current_user.teams.first
+  def set_working_team
+    @working_team = current_user.teams.first
   end
 end
