e19c0100dd29995002b5cc0bbb7a5312b693544f
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-09-26 16:45:11 +0900

    見本のテストとしてUser.find_or_create_by_emailのテストを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '引数で送られてきたアドレスを検索して、Userをcreateもしくはfindする' do
+    let!(:created_user) { User.create(email: 'created_user@example.com', password: '199392199392') }
+    let!(:not_created_user) { User.find_or_create_by_email('not_created_user@example.com') }
+
+    it '引数で送られてきたアドレスを持っているUserがいたら、そのUserをfindしてインスタンスを返り値で返す' do
+      expect(User.find_or_create_by_email('created_user@example.com')).to eq created_user
+    end
+
+    it '引数で送られてきたアドレスを持っているUserがいなかったら、Userをcreateして、そのインスタンスを返す' do
+      expect(not_created_user.persisted?).to eq true
+      expect(not_created_user.encrypted_password.present?).to eq true
+    end
+  end
 end
