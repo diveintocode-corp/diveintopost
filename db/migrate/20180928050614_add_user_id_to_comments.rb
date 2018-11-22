b41876baae312e4b83340913348cc63bc1f6fe94
芝田大樹 <shibatadaiki@shibatadaikinoMacBook-Air.local>
2018-11-22 13:17:37 +0900

    間違ったマイグレーションファイルの削除

--- db/migrate/20180928050614_add_user_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToComments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :comments, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -52,10 +52,11 @@ ActiveRecord::Schema.define(version: 2018_10_05_035232) do
   create_table "comments", force: :cascade do |t|
     t.bigint "article_id"
     t.text "content"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "teams", force: :cascade do |t|
