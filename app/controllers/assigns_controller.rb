42fa594022ffa6f86c654fca2b53d9527d5c6008
daiki shibata <shibatadaiki92@gmail.com>
2018-12-20 13:17:12 +0900

    Update assigns_controller.rb

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -14,17 +14,9 @@ class AssignsController < ApplicationController
 
   def destroy
     assign = Assign.find(params[:id])
-    assigned_user = assign.user
-    if assigned_user == assign.team.owner
-      redirect_to team_url(params[:team_id]), notice: 'リーダーは削除できません。'
-    elsif Assign.where(user_id: assigned_user.id).count == 1
-      redirect_to team_url(params[:team_id]), notice: 'このユーザーはこのチームにしか所属していないため、削除できません。'
-    else
-      another_team = Assign.find_by(user_id: assigned_user.id).team
-      change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
-      assign.destroy
-      redirect_to team_url(params[:team_id]), notice: 'メンバーを削除しました。'
-    end
+    destroy_message = assign_destroy(assign, assign.user)
+
+    redirect_to team_url(params[:team_id]), notice: destroy_message
   end
 
   private
@@ -33,7 +25,25 @@ class AssignsController < ApplicationController
     params[:email]
   end
 
+  def assign_destroy(assign, assigned_user)
+    if assigned_user == assign.team.owner
+      'リーダーは削除できません。'
+    elsif Assign.where(user_id: assigned_user.id).count == 1
+      'このユーザーはこのチームにしか所属していないため、削除できません。'
+    elsif assign.destroy
+      set_next_team(assign, assigned_user)
+      'メンバーを削除しました。'
+    else
+      'なんらかの原因で、削除できませんでした。'
+    end    
+  end  
+  
   def email_reliable?(address)
     address.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+  end
+  
+  def set_next_team(assign, assigned_user)
+    another_team = Assign.find_by(user_id: assigned_user.id).team
+    change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 end
