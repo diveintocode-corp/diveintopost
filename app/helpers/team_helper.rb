5b7b9f1fe7acf3ac7adb53af605fcafbb1c77feb
setsuotokohorikawa <ralralph@gmail.com>
2019-09-24 16:10:13 +0900

    add:ownerでアクセスした場合にのみ、権限移動ボタンを表示

--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -2,4 +2,9 @@ module TeamHelper
   def default_img(image)
     image.presence || 'default.jpg'
   end
+
+  def is_owner?
+    team = Team.friendly.find(params[:id])
+    current_user.id == team.owner_id
+  end
 end
