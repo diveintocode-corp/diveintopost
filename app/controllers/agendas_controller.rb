f37d6f2099f8d714016581f4a3d83803c568377f
yuji91 <yuji91.1227@gmail.com>
2020-06-14 13:29:06 +0900

    rubocop -a

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -15,7 +15,7 @@ class AgendasController < ApplicationController
     @agenda.team = Team.friendly.find(params[:team_id])
     current_user.keep_team_id = @agenda.team.id
     if current_user.save && @agenda.save
-      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda') 
+      redirect_to dashboard_url, notice: I18n.t('views.messages.create_agenda')
     else
       render :new
     end
--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -37,11 +37,11 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_member_4_some_reason')
     end
   end
-  
+
   def email_reliable?(address)
     address.match(/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
   end
-  
+
   def set_next_team(assign, assigned_user)
     another_team = Assign.find_by(user_id: assigned_user.id).team
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
